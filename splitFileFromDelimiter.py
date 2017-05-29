65f5f07de8d9c90530768008ff2b7f3c128c85fd
Jesus Renero Quintero <jesus.renero@gmail.com>
2017-05-29 10:41:55 +0200

    files removal

--- splitFileFromDelimiter.py
+++
@@ -1,8 +0,0 @@
-import itertools as it
-filename='test.dat'
-
-with open(filename,'r') as f:
-    for key,group in it.groupby(f,lambda line: line.startswith('=======')):
-        if not key:
-            group = list(group)
-            print(group)
